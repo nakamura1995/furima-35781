f5db7c3fb3428e540e85c307cd78e366b746e41c
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2021-09-25 16:21:25 +0900

    hasmanyの記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :nickname, presence: true
   validates :encrypted_password, presence: true, length: { maximum: 6 }
+
+  has_many :items
+  has_many :records
 end
