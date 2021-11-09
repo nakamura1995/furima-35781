0342551857abe8ff372fca41705d9767207a863e
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2021-11-09 17:50:33 +0900

    モデルテストなど

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-root to: "items#index"
+root to: "users#index"
   devise_for :users
   resources :items, only: [:index]
-  resources :users, only: [:index, :edit]
+  resources :users, only: [:index, :edit,:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,16 +6,25 @@ RSpec.describe User, type: :model do
   end
 end
 
-describe 'ユーザー新規登録' do
+describe "ユーザー新規登録" do
   it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+    expect(@user).to be_valid
   end
+
   it 'nicknameが空では登録できない' do
-    user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
-       user.valid?
+       @user.nickname = ""
+       @user.valid?
+       expect(@user.errors.full.messages).to include("Nickname can't be blank")
   end
+
   it 'emailが空では登録できない' do
-  end
+   @user.email = ""
+   @user.valid?
+   expect(@user.errors.full.messages).to include("Email can't be blank")
+  end 
   it 'passwordが空では登録できない' do
+    @user.password = ""
+    @user.valid?
   end
   it 'passwordとpassword_confirmationが不一致では登録できない' do
   end
@@ -28,6 +37,6 @@ describe 'ユーザー新規登録' do
   end
   it 'passwordが5文字以下では登録できない' do
   end
-end
+
 end
 
