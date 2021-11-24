32eb8f00f8be21b43d88908dcf88321fac437e32
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2021-11-24 17:58:07 +0900

    itemsテーブルの作成

--- Gemfile
+++ Gemfile
@@ -60,3 +60,5 @@ group :development do
   gem 'rubocop', require: false
 end
 gem 'devise'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- db/migrate/20211120093835_devise_create_items.rb
+++ db/migrate/20211120093835_devise_create_items.rb
@@ -4,9 +4,17 @@ class DeviseCreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
       
+      t.string :name,          null: false
+      t.integer :category_id,   null: false
+      t.integer :price,        null: false
+      t.integer :explanation_id,     null: false
+      t.integer :status_id,      null: false
+      t.integer :days_id,       null: false
+      t.references :user, foreign_key: true
+      t.text :description,   null:false
+      t.integer :prefectures_id ,    null:false
+
 
       ## Recoverable
       t.string   :reset_password_token
