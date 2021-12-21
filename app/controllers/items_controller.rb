cb7203b0da03fa67dd83cfa33c09634249996b1d
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2021-12-21 18:59:16 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :move_to_index, except: [:index, :show, :require_login]
+  before_action :authenticate_user!
  #before_action :set_item, only: [:show, :edit, :update]
   
    def index
@@ -28,12 +28,7 @@ def item_params
   params.require(:item).permit( :image, :name, :category_id, :price, :explanation_id, :status_id, :days_id, :description, :prefectures_id, :user_id).merge(user_id: current_user.id)
 end
 
-def move_to_index
-  redirect_to new_user_session_path unless user_signed_in?
-end
 
-# def set_item
-#   @items = Item.find(params[:id])
-# end
+
 
 end
