82b345103793638c3261fcee2dfdf0e2951967fa
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2022-01-10 17:18:29 +0900

    商品編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
   
 
   def index
@@ -14,7 +15,16 @@ class ItemsController < ApplicationController
   def show  
   end
 
- 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
 
   def create
     @item = Item.new(item_params)
@@ -34,5 +44,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :category_id, :price, :explanation_id, :status_id, :day_id, :description,
                                  :prefecture_id).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user.id == @item.user.id
   end
 end
