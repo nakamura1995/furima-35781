0128c7fe7d3a0b3ce8e89eeee8aa9fd222d77629
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2022-02-04 14:09:15 +0900

    購入機能実装2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,9 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
+    
         if @order_address.valid?
+          pay_item
           @order_address.save
           redirect_to root_path
         else
@@ -24,7 +26,7 @@ class OrdersController < ApplicationController
 
   def order_params
     item = Item.find(params[:item_id])
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone).merge(user_id: current_user.id, item_id: item.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone).merge(user_id: current_user.id, item_id: item.id, token: params[:token])
   end
 
   def set_item
@@ -33,6 +35,15 @@ class OrdersController < ApplicationController
   
   def contributor_confirmation
     redirect_to root_path if current_user.id == @item.user.id
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_89958d3f8ca48da15757ab0b"   # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
   # def sold_out
