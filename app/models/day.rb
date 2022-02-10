97ecd1d5d2faec7c24c88e8fed03aa073efb88a2
shokichi0801 <85923227+shokichi0801@users.noreply.github.com>
2022-02-10 22:30:14 +0900

    修正

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,9 +2,9 @@ class Day < ActiveHash::Base
   self.data = [
 
     { id: 1, name: '--' },
-    { id: 2, name: '1日〜2日で発送' },
-    { id: 3, name: '2日〜３日で発送' },
-    { id: 4, name: '4日〜7日で発送' }
+    { id: 2, name: '1日~2日で発送' },
+    { id: 3, name: '2日~３日で発送' },
+    { id: 4, name: '4日~7日で発送' }
   ]
 
   include ActiveHash::Associations
